b64ec869abc1dcfab0bfa861b048541dafc69699
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-04-02 17:09:18 -0400

    todo-list Updated

---
+++ scripts/todo-list-upg.js
@@ -0,0 +1,54 @@
+const todoList = [];
+
+function addTodo(){
+    const inputName = document.querySelector('.js-name-input');
+    const inputDate = document.querySelector('.js-date-input');
+
+    const name = inputName.value;
+    const dueDate = inputDate.value;
+    
+    todoList.push({name, dueDate});
+
+    inputName.value = '';
+    inputDate.value = '';
+}
+
+function renderTodoList(){
+    let todoListHTML = '';
+
+    todoList.forEach(function(todoElement, index){
+        const html = `
+            <div>${todoElement.name}</div>
+            <div>${todoElement.dueDate} </div>
+            <button class="delete-button" onclick="
+                todoList.splice(${index}, 1);
+                renderTodoList();
+            ">Delete</button>
+            `;
+        todoListHTML += html;
+    });
+    // for (let i = 0; i < todoList.length; i++){
+    //     const html = `
+    //         <div>${todoList[i].name}</div>
+    //         <div>${todoList[i].dueDate} </div>
+    //         <button class="delete-button" onclick="
+    //             todoList.splice(${i}, 1);
+    //             renderTodoList();
+    //         ">Delete</button>
+    //         `;
+    //     todoListHTML += html;
+    // }
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+document.body.addEventListener('keydown',(event) => {
+    if(event.key === 'Enter'){
+        addTodo(); 
+        renderTodoList();
+    }
+});
+
+document.querySelector('.js-add-button').addEventListener('click',() => {
+    addTodo(); 
+    renderTodoList();
+});
