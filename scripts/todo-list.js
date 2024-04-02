8b81ac0ebf4ddd97ca28a6eae701f29a82061b15
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-04-02 00:24:23 -0400

    Updated

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,18 +1,32 @@
 const todoList = [];
 
 function addTodo(){
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
-    todoList.push(name);
+    const inputName = document.querySelector('.js-name-input');
+    const inputDate = document.querySelector('.js-date-input');
 
-    inputElement.value = '';
+    const name = inputName.value;
+    const dueDate = inputDate.value;
+    
+    todoList.push({name, dueDate});
+
+    console.log(todoList);
+
+    inputName.value = '';
+    inputDate.value = '';
 }
 
 function renderTodoList(){
     let todoListHTML = '';
 
     for (let i = 0; i < todoList.length; i++){
-        const html = `<p>${todoList[i]}</p>`;
+        const html = `
+            <p>
+                ${todoList[i].name} ${todoList[i].dueDate} 
+                <button onclick="
+                    todoList.splice(${i}, 1);
+                    renderTodoList();
+                ">Delete</button>
+            </p>`;
         todoListHTML += html;
     }
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
