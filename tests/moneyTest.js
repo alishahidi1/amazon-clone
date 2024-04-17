babd8e405d9181fc81447f373723509889dfe3a0
alishahidi1 <ali.shahidi@uwaterloo.ca>
2024-04-17 12:40:01 -0400

    Automated test is added to test format currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,25 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+if (formatCurrency(2095) === '20.95'){
+    console.log('Passed!');
+} else {
+    console.log('Failed');
+}
+
+if (formatCurrency(0) === '0.00') {
+    console.log('Passed!');
+} else {
+    console.log('Failed!!!');
+}
+
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('Passed!');
+} else {
+    console.log('Failed!!!');
+}
+
+if (formatCurrency(2000.4) === '20.00') {
+    console.log('Passed!');
+} else {
+    console.log('Failed!!!');
+}
